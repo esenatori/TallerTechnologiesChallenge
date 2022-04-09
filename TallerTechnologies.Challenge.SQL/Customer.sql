b687684d1b4247f9ead19baa6c9afb15956a0329
EMMANUEL SENATORI <senatori.emmanuel@gmail.com>
2022-04-09 12:17:33 -0300

    Table Customer

---
+++ TallerTechnologies.Challenge.SQL/Customer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[PersonID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] varchar(500) NULL, 
+    [LastName] varchar(500) NULL, 
+    [Age] int NULL, 
+    [Occupation] varchar(500) NULL, 
+    [MartialStatus] bit NULL,
+    
+)
